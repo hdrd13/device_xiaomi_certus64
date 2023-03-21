10081eeb9ee9580b6e6f70f2ac54a72b4d296f64
sayann70 <sayansarkarda2@gmail.com>
2023-03-21 18:12:05 +0100

    certus64: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Mayuri-Chan/proprietary_vendor_xiaomi -b 11-alps vendor/xiaomi
+git clone https://github.com/Mayuri-Chan/android_kernel_xiaomi_mt6765 kernel/xiaomi/mt6765
