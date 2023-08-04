e0b400e16f034c99dd7a560fd2947f59a3ddf71c
sayann70 <sayansarkarda2@gmail.com>
2023-08-04 21:23:23 +0530

    certus: add vendorsetup.sh
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/sayann70/vendor_xiaomi -b 12 vendor/xiaomi
+git clone https://github.com/Mayuri-Chan/android_kernel_xiaomi_mt6765 -b 10-alps kernel/xiaomi/mt6765
+git clone https://gitlab.com/wulan17/linaro_arm-linux-gnueabihf-7-5 prebuilts/gcc/linux-x86/arm/arm-linux-gnueabihf-7.5
