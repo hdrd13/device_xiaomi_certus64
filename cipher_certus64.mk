b7a2091b910dc02af288e9b73f54779831477c94
sayann70 <sayansarkarda2@gmail.com>
2023-08-05 10:56:37 +0530

    certus: CipherOS bringup
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- cipher_certus64.mk
+++ cipher_certus64.mk
@@ -17,18 +17,18 @@
 # Inherit from device
 $(call inherit-product, device/xiaomi/certus64/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Cipher stuff.
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
-# Official tag
-NAD_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+CIPHER_MAINTAINER := sayann70
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := certus64
-PRODUCT_NAME := nad_certus64
+PRODUCT_NAME := cipher_certus64
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi 6 / Redmi 6A
 PRODUCT_MANUFACTURER := Xiaomi
